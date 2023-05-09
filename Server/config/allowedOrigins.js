8108ff6d3452fb61a7ddedd51e3353a3ffcc7f40
Onyewuchi Emeka <marrnuel123@.com>
2023-05-09 19:00:57 +0100

    first commit -includes user & notes model, errorHandlers, and cors config

---
+++ Server/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://www.manuel.com',
+    'https:// manuel.com',
+
+]
+
+module.exports = allowedOrigins
---
+++ Server/server.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const { logEvents } = require("./middleware/logger");
+const PORT = process.env.PORT || 3000;
+
+console.log(process.env.PORT);
+connectDB();
+
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+// middleware
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", require("./routes/root.js"));
+
+// handling server errors
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found ");
+  }
+});
+
+//errorHandler
+app.use(errorHandler);
+
+// listening on port
+
+mongoose.connection.once("open", () => {
+  console.log("Connected To MongoDB");
+  app.listen(PORT, () => {
+    return console.log(`Server is running on port ${PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
