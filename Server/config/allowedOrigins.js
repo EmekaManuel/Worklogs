8bb42bf646d4554f8aa33a976ccb9646a0566705
Onyewuchi Emeka <marrnuel123@.com>
2023-05-12 08:29:36 +0100

    Successfully connected the frontend to the backend

--- Server/config/allowedOrigins.js
+++ Server/config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
-    'http://localhost:5173',
     'https://www.manuel.com',
-    'https:// manuel.com',
+    'https://manuel.com',
+    'https://manuel.com',
+    'http://localhost:5173',
+    'https://localhost:5173'
 
 ]
 
