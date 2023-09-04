34c20d1dca5f16197d8d0e6ab7ceaad2457bca5f
Onyewuchi Emeka <marrnuel123@.com>
2023-09-05 00:28:25 +0100

    refactor(): refactored the redux and client-side application

--- Client/src/features/users/User.jsx
+++ Client/src/features/users/User.jsx
@@ -1,13 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import { memo } from "react";
 
-import { useSelector } from "react-redux";
-import { selectUserById } from "./usersApiSlice";
+import { useGetUsersQuery } from "./usersApiSlice";
 
-// eslint-disable-next-line react/prop-types
+// eslint-disable-next-line react/prop-types, react-refresh/only-export-components
 const User = ({ userId }) => {
-  const user = useSelector((state) => selectUserById(state, userId));
+  const { user } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => ({
+      user: data?.entities[userId],
+    }),
+  });
 
   const navigate = useNavigate();
 
@@ -31,4 +35,5 @@ const User = ({ userId }) => {
     );
   } else return null;
 };
-export default User;
+const memoizedUser = memo(User);
+export default memoizedUser;
